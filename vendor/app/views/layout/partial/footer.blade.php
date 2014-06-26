be3bb10cf397e7cbcaa814c904eb9fc226327809
zendzzo <zendzzo@gmail.com>
2014-06-26 15:05:31 +0700

    remove some Bugs

--- vendor/app/views/layout/partial/footer.blade.php
+++
@@ -1,8 +0,0 @@
-        <!-- jQuery 2.0.2 -->
-        {{ HTML::script('/assets/js/jquery-2.0.2.min.js') }}
-        <!-- Bootstrap -->
-        {{ HTML::script('/assets/js/bootstrap.min.js') }}
-        <!-- AdminLTE App -->
-        {{ HTML::script('assets/js/AdminLTE/app.js') }}
-    </body>
-</html>
